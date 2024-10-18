0ebd20193e5001582affb33870be891ec8422599
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-10-18 20:24:06 +0100

    updated the js file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,26 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', ()=>{
    
      const NewQuote =document.getElementById('newQuote');
      const DisplayQuotes =document.getElementById('quoteDisplay');
-   const quotes=[
-      "Believe you can and you're halfway there. – Theodore Roosevelt",
-"Do what you can, with what you have, where you are. – Theodore Roosevelt",
-"Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
-"What lies behind us and what lies before us are tiny matters compared to what lies within us. – Ralph Waldo Emerson",
-"In the middle of every difficulty lies opportunity. – Albert Einstein",
-"Happiness is not something ready made. It comes from your own actions. – Dalai Lama",
-"Don’t watch the clock; do what it does. Keep going. – Sam Levenson",
-"It always seems impossible until it’s done. – Nelson Mandela",
-"You are never too old to set another goal or to dream a new dream. – C.S. Lewis",
-"Act as if what you do makes a difference. It does. – William James"
-
-   ];
+      const quotes = [
+         {category:"love", text:"Love conquers all."},
+         {category:"love", text:"you know you're in love when you can't fall asleep because reality is finally better than your dreams-Dr Seuss"},
+         {category:"love", text:"Love is composed of a single soul inhabiting two bodies- Aristole"},
+         {category:"motivation", text:"just do it"},
+         {category:"motivation", text:"i'm not arguing i'm just explaining why i'm right"},
+         {category:"motivation", text:"Behind  every great man is a woman rolling her eyes-Jim carrey "},
+         {category:"humor",text:"i'm on a seafood diet,I see food and i eat it "},
+         {category:"motivation", text:"The best way to get started is to quit talking and begin doing- walt Disney"},
+         {category:"motivation", text:"success is not final,failure is not fata:it is the courage to continue that counts-Winston Churchill"},
+         {category:"motivation", text:"Dont't wartch the clock, do what it does keep going- sam levenson"}
+      ];
 
 
      function addQuotes(){
       const randomposition =Math.floor(Math.random() * quotes.length);
       const randomquote =quotes[randomposition];
-      DisplayQuotes.innerHTML=randomquote;
+      DisplayQuotes.innerHTML = `
+      Category: ${randomquote.category}
+       <p>${randomquote.text}</p>
+  `;
    }
 
 
