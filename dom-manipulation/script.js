fa21c14150ab7fc490693eb78b8a55b33f0f59f5
Frank Utibe Joseph <majesticfrank7@gmail.com>
2024-10-20 00:42:43 +0100

    updated

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -55,43 +55,43 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 
-// let quotes = [
-//    { text: "The best way to predict the future is to invent it.", category: "Innovation" },
-//    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
-//    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" }
-// ];
+let quotes = [
+   { text: "The best way to predict the future is to invent it.", category: "Innovation" },
+   { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+   { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" }
+];
 
-// function showRandomQuote() {
-//    const randomIndex = Math.floor(Math.random() * quotes.length);
-//    const quote = quotes[randomIndex];
+function showRandomQuote() {
+   const randomIndex = Math.floor(Math.random() * quotes.length);
+   const quote = quotes[randomIndex];
    
-//    const quoteDisplay = document.getElementById("quoteDisplay");
-//    quoteDisplay.innerHTML = `<p><strong>Quote:</strong> ${quote.text}</p><p><strong>Category:</strong> ${quote.category}</p>`;
-// }
+   const quoteDisplay = document.getElementById("quoteDisplay");
+   quoteDisplay.innerHTML = `<p><strong>Quote:</strong> ${quote.text}</p><p><strong>Category:</strong> ${quote.category}</p>`;
+}
 
-// function addQuote() {
-//    const newQuoteText = document.getElementById("newQuoteText").value;
-//    const newQuoteCategory = document.getElementById("newQuoteCategory").value;
+function addQuote() {
+   const newQuoteText = document.getElementById("newQuoteText").value;
+   const newQuoteCategory = document.getElementById("newQuoteCategory").value;
 
 
-//    if (newQuoteText.trim() === "" || newQuoteCategory.trim() === "") {
-//        alert("Please enter both a quote and a category.");
-//        return;
-//    }
+   if (newQuoteText.trim() === "" || newQuoteCategory.trim() === "") {
+       alert("Please enter both a quote and a category.");
+       return;
+   }
 
-//    const newQuote = {
-//        text: newQuoteText,
-//        category: newQuoteCategory
-//    };
-//    quotes.push(newQuote);
+   const newQuote = {
+       text: newQuoteText,
+       category: newQuoteCategory
+   };
+   quotes.push(newQuote);
 
  
-//    document.getElementById("newQuoteText").value = "";
-//    document.getElementById("newQuoteCategory").value = "";
+   document.getElementById("newQuoteText").value = "";
+   document.getElementById("newQuoteCategory").value = "";
 
   
-//    showRandomQuote();
-// }
+   showRandomQuote();
+}
 
 
-// document.addEventListener("DOMContentLoaded", showRandomQuote);
+document.addEventListener("DOMContentLoaded", showRandomQuote);
